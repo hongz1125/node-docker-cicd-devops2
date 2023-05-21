13a3ef204943497a80c88920589623fd8e800006
zhihong <hongz1125@qq.com>
2023-05-21 22:51:29 +0800

    up3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 http
   .createServer((req, res) => {
-    res.end(`web ready2!!!`);
+    res.end(`web ready3!!!`);
   })
   .listen(9001, () => {
     console.log("web server start");
