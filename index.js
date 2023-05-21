e8f2104677efe1177858408e55b950156df05e66
zhihong <hongz1125@qq.com>
2023-05-21 22:56:38 +0800

    feat: up4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 http
   .createServer((req, res) => {
-    res.end(`web ready3!!!`);
+    res.end(`web ready4!!!`);
   })
   .listen(9001, () => {
     console.log("web server start");
